f84e827c820ea2c23a9b09d0fb9dfed3cd629980
Aman Badone <81856257+amanbadone@users.noreply.github.com>
2023-10-18 15:53:58 +0000

    reads redirect file

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 //1.load all the url from redirects.yml
-const YAML = require('yaml')
+// yaml module object
+const YAML = require('yaml') 
+// fs stands for file system  object
+const fs = require('fs') 
+//require path
+const path = require('path')
+
+
+const redirectsFile = fs.readFileSync(path.join(__dirname,'redirects.yml'), 'utf-8')
+
+console.log(redirectsFile)
 //2.Create a html page for each redirect page from template.html
